325ba86a40f2cabe8a9be163f8902be008513f97
Seney Alvarado Carvajal <seney.alvarado@ucrso.info>
2019-06-13 13:59:14 -0600

    cambio de nombres masterAdmin

--- resources/views/masterAdmin.blade.php
+++ resources/views/masterAdmin.blade.php
@@ -1,119 +1,196 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Administrador Sistema de Monitoreo de Procesos - Grupo Prisma</title>
-	@yield('encabezado')
-	
-    <link rel="stylesheet" type="text/css" href="{{asset('css/menus.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/masterPage.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/glyphicons-align.css')}}">
-    <script src="{{asset('js/toggle.js')}}"></script>
-	<link rel="stylesheet" type="text/css" href="{{asset('css/paneles.css')}}">
-	<script src="{{asset('//code.jquery.com/jquery-1.11.1.min.js')}}"></script>
-	<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://revistas.ucr.ac.cr/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    <link href="./css/base.css" rel="stylesheet">
-    <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
-
-	<script async="" src="//www.google-analytics.com/analytics.js"></script><script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
-    <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
-    <script id="_carbonads_projs" type="text/javascript" src="//srv.carbonads.net/ads/CK7DC5QN.json?segment=placement:eonasdangithubio&amp;callback=_carbonads_go"></script></head>
-	
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Sistema de Monitoreo de Procesos - Grupo Prisma</title>
+	<!-- Bootstrap core CSS 
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="{{asset('css/simple-sidebar.css')}}">
+    <link rel="stylesheet" href="{{asset('css/master-root.css')}}">
+    <link rel="stylesheet" href="{{asset('css/glyphicons.css')}}">
+    <!--<script src="{{asset('js/master-root.js')}}"></script>-->
+    <script src="{{asset('js/menus_dinamicos.js')}}"></script>
+	<script src="{{asset('js/app.min.js')}}"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>  
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="css/simple-sidebar.css" rel="stylesheet">
 </head>
-<body lcass="body">
-<nav class="navbar navbar-default navbar-static-top ">
-  <div class="container-fluid">
-  <a class="navbar-toggler pull-right" href="#menu-toggle" id="menu-toggle"><span style=" font-size: 25px; margin-top:13px; color:white;" class="glyphicon glyphicon-menu-hamburger"></a>
-    <a class="navbar-brand font-color" style="margin-left:50px" href="#">Sistema de Monitoreo</a>
-  </div>
-</nav>
-   <div id="wrapper" >
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-		
-            <ul class="sidebar-nav">
-				
-            		<li>
-		      			<a class="border font-color" href="{{ url('admin_accounts_index') }}">Cuentas<span class="glyphicon glyphicon-user right-aling-glyphicon-cuentas"></a>
-					</li>
-					<li>
-		      			<a class="border font-color" href="{{ url('productIndex')}}">Trabajos<span class="glyphicon glyphicon-folder-open right-aling-glyphicon-trabajos"></a>
-		    		</li>
-					<li>
-		      			<a class="border font-color" href="{{ url('clients') }}">Clientes<span class="glyphicon glyphicon-comment right-aling-glyphicon-clientes"></a>
-		    		</li>
-		    		<li>
-		      			<a class="border font-color" href="{{ url('estados') }}">Estados<span class="glyphicon glyphicon glyphicon-tags right-aling-glyphicon-estados"></a>
-		    		</li>
-		    		<li>
-		      			<a class="border font-color" href="{{ url('visitas') }}">Visitas<span class="glyphicon glyphicon-home right-aling-glyphicon-visitas"></a>
-		    		</li>
-					<li>
-						<a class="border font-color" href="{{ url('contrasennaAdmin') }}">Contraseña<span class="glyphicon glyphicon-lock right-aling-glyphicon-contrasenna"></a>
-				  	</li>
-		    		<li class="hide-button-side">
-		      			<a class="border font-color" href="{{ url('/logout') }}">Salir<span class="glyphicon glyphicon-log-out right-aling-glyphicon-logout"></a>
-                    </li>
-                    <br>
-                    <br>
-		    		<li>	
-      					<div class="logo_prisma_sidebar"><img src="{{asset('Imagenes/prisma_logo.png')}}"style="width:120px; height:120px;"></div>
-      				</li>    			
-            </ul>
-        </div>
 
-        
-        <!-- /#sidebar-wrapper -->
-
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-
-                    @yield('contenido_Admin')	
-                    @yield('content')
-                    @if(session('error'))
-                    <div class="alert alert-danger alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        {{@session('error')}}
-                    </div>
-                    @endif
-                    </div>
-                </div>
-            </div>
-                    <!-- /#page-content-wrapper -->
-        <footer class="main-footer" style="text-align:center;">
-            <div class="text-center " >
-                <a style="font-size:1em; color:#30A8D8; "><strong>Grupo Prisma ©2019</strong></a><img style="margin-top: 4px;" class="margin-logo" src="{{asset('Imagenes/prisma_footer.png')}}" >
-            </div>
-            </footer>
-        </div>
-
-        </div>
+<body>
+<nav class="navbar nav-color navbar-expand-md navbar-dark bg-primary border border-left-0 border-top-0 border-right-0 border-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">
+    <img src="{{asset('Imagenes/logo.png')}}" width="35" height="35" class="d-inline-block align-top"></div>  
+    <span class="menu-collapsed">Grupo Prisma</span>
+  </a>
+  
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item a active">
+        <button class="btn btn-default style-return-button" href="#"><span class="glyphicon glyphicon-menu-left"></span></button>
+      </li>
       
+      <!-- This menu is hidden in bigger devices with d-sm-none. 
+        The sidebar isn't proper for smaller screens imo, so this dropdown menu can keep all the useful sidebar itens exclusively for smaller screens  -->
+      <li class=" nav-item dropdown d-sm-block d-md-none">
+        <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menú
+        </a>
+        <div class="dropdown-menu" aria-labelledby="smallerscreenmenu">
+            <a class="dropdown-item" href="#">Trabajos</a>
+            <a class="dropdown-item" href="#">Estados</a>
+            <a class="dropdown-item" href="#">Cuentas</a>
+        </div>
+      </li><!-- Smaller devices menu END -->
+      
+    </ul>
+  </div>
+
+  <div class="dropdown">
+  <button type="button" class="btn btn-default dropdown-toggle style-name-button"  data-toggle="dropdown">
+    Nombre del mae
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Link 1</a>
+    <a class="dropdown-item" href="#">Link 2</a>
+    <a class="dropdown-item" href="#">Link 3</a>
+  </div>
+</div>
+
+</nav><!-- NavBar END -->
+
+<!-- Bootstrap row -->
+<div class="row" id="body-row">
+    <!-- Sidebar -->
+    <div id="sidebar-container" class="sidebar-expanded d-none d-md-block"><!-- d-* hiddens the Sidebar in smaller devices. Its itens can be kept on the Navbar 'Menu' -->
+        <!-- Bootstrap List Group -->
+        <ul class="list-group">
+            <!-- Menu with submenu -->
+            <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="border border-left-0 border-top-0 border-right-0 border-light sidebar-color list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-start align-items-center">
+                    <span class="glyphicon glyphicon-folder-open fa-fw mr-3"></span> 
+                    <span class="menu-collapsed">Trabajos</span>
+                    <span class="glyphicon glyphicon-menu-down submenu-icon ml-auto"></span>
+                </div>
+            </a>
+            <!-- Submenu content -->
+            <div id='submenu1' class="collapse sidebar-submenu">
+                <a href="#" class=" list-group-item list-group-item-action sidebar-color-collapse text-white">
+                    <span class="menu-collapsed">Visualizar</span>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action sidebar-color-collapse text-white">
+                    <span class="menu-collapsed">Estados</span>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action sidebar-color-collapse text-white">
+                    <span class="menu-collapsed">Materiales</span>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action sidebar-color-collapse sidebar-color text-white">
+                    <span class="menu-collapsed">Productos</span>
+                </a>
+            </div>
+            <a href="#" class="sidebar-color  list-group-item list-group-item-action border border-left-0 border-right-0 border-light" >
+                <div class="d-flex w-100 justify-content-start align-items-center">
+                    <span class="glyphicon glyphicon-user fa-fw mr-3"></span>
+                    <span class=" menu-collapsed">Clientes</span>    
+                </div>
+            </a>
+            <a href="{{ url('admin_accounts_index') }}" class="border border-left-0 border-right-0 border-light sidebar-color  list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-start align-items-center">
+                    <span class="glyphicon glyphicon-list fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Cuentas</span>
+                </div>
+            </a>
+            <a href="#" class="sidebar-color border border-light border-left-0 border-right-0 list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-start align-items-center">
+                    <span class="glyphicon glyphicon-file fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Visitas</span>
+                </div>
+            </a>
+            <a href="#" data-toggle="sidebar-colapse" class="border border-left-0 border-right-0 border-light active-collapse sidebar-color  list-group-item list-group-item-action d-flex align-items-center">
+                <div class="d-flex w-100 justify-content-start align-items-center">
+                    <span id="collapse-icon" class="glyphicon glyphicon-resize-horizontal fa fa-2x mr-3"></span>
+                    <span id="collapse-text" class="menu-collapsed">Cerrar menú</span>
+                </div>
+            </a>
+            <!-- Logo -->
+            <li class="list-group-item logo-separator d-flex justify-content-center">
+                <!-- <img src='https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg' width="30" height="30" />  -->  
+            </li>   
+        </ul><!-- List Group END-->
+    </div><!-- sidebar-container END -->
+
+    <!-- MAIN -->
+    <div class="col">
+    <div class="container-fluid">
+        @if(session('error'))
+        <div class="alert alert-danger alert-dismissible" style="text-align: center; padding:20px;" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          {{@session('error')}}
+        </div>
+        @endif
+        @if(session('success'))
+        <div class="alert alert-success alert-dismissible" style="text-align: center; margin-top:10px; 
+                    margin-left:10px; margin-right:10px;" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          {{@session('success')}}
+        </div>
+        @endif
+        @if(session('info'))
+        <div class="alert alert-info alert-dismissible" style="text-align: center;" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          {{@session('info')}}
+        </div>
+        @endif
+        @yield('contenido_Admin')
+    </div>
+    </div><!-- Main Col END -->
+    </div><!-- body-row END -->
+
+<script>
+// Hide submenus
+$('#body-row .collapse').collapse('hide'); 
+
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left'); 
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+    
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+    
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
+</script>
 </body>
-    <!-- /#wrapper -->
-     <!-- Menu Toggle Script -->
-    <script>
-    $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-	});
-	
-    </script>
-<html>
+
+</html>
---
+++ resources/views/masterAdminViejo.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Administrador Sistema de Monitoreo de Procesos - Grupo Prisma</title>
+	@yield('encabezado')
+	
+    <link rel="stylesheet" type="text/css" href="{{asset('css/menus.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/masterPage.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/glyphicons-align.css')}}">
+    <script src="{{asset('js/toggle.js')}}"></script>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/paneles.css')}}">
+	<script src="{{asset('//code.jquery.com/jquery-1.11.1.min.js')}}"></script>
+	<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://revistas.ucr.ac.cr/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <link href="./css/base.css" rel="stylesheet">
+    <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+
+	<script async="" src="//www.google-analytics.com/analytics.js"></script><script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+    <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
+    <script id="_carbonads_projs" type="text/javascript" src="//srv.carbonads.net/ads/CK7DC5QN.json?segment=placement:eonasdangithubio&amp;callback=_carbonads_go"></script></head>
+	
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+</head>
+<body lcass="body">
+<nav class="navbar navbar-default navbar-static-top ">
+  <div class="container-fluid">
+  <a class="navbar-toggler pull-right" href="#menu-toggle" id="menu-toggle"><span style=" font-size: 25px; margin-top:13px; color:white;" class="glyphicon glyphicon-menu-hamburger"></a>
+    <a class="navbar-brand font-color" style="margin-left:50px" href="#">Sistema de Monitoreo</a>
+  </div>
+</nav>
+   <div id="wrapper" >
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+		
+            <ul class="sidebar-nav">
+				
+            		<li>
+		      			<a class="border font-color" href="{{ url('admin_accounts_index') }}">Cuentas<span class="glyphicon glyphicon-user right-aling-glyphicon-cuentas"></a>
+					</li>
+					<li>
+		      			<a class="border font-color" href="{{ url('productIndex')}}">Trabajos<span class="glyphicon glyphicon-folder-open right-aling-glyphicon-trabajos"></a>
+		    		</li>
+					<li>
+		      			<a class="border font-color" href="{{ url('clients') }}">Clientes<span class="glyphicon glyphicon-comment right-aling-glyphicon-clientes"></a>
+		    		</li>
+		    		<li>
+		      			<a class="border font-color" href="{{ url('estados') }}">Estados<span class="glyphicon glyphicon glyphicon-tags right-aling-glyphicon-estados"></a>
+		    		</li>
+		    		<li>
+		      			<a class="border font-color" href="{{ url('visitas') }}">Visitas<span class="glyphicon glyphicon-home right-aling-glyphicon-visitas"></a>
+		    		</li>
+					<li>
+						<a class="border font-color" href="{{ url('contrasennaAdmin') }}">Contraseña<span class="glyphicon glyphicon-lock right-aling-glyphicon-contrasenna"></a>
+				  	</li>
+		    		<li class="hide-button-side">
+		      			<a class="border font-color" href="{{ url('/logout') }}">Salir<span class="glyphicon glyphicon-log-out right-aling-glyphicon-logout"></a>
+                    </li>
+                    <br>
+                    <br>
+		    		<li>	
+      					<div class="logo_prisma_sidebar"><img src="{{asset('Imagenes/prisma_logo.png')}}"style="width:120px; height:120px;"></div>
+      				</li>    			
+            </ul>
+        </div>
+
+        
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                    @yield('contenido_Admin')	
+                    @yield('content')
+                    @if(session('error'))
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        {{@session('error')}}
+                    </div>
+                    @endif
+                    </div>
+                </div>
+            </div>
+                    <!-- /#page-content-wrapper -->
+        <footer class="main-footer" style="text-align:center;">
+            <div class="text-center " >
+                <a style="font-size:1em; color:#30A8D8; "><strong>Grupo Prisma ©2019</strong></a><img style="margin-top: 4px;" class="margin-logo" src="{{asset('Imagenes/prisma_footer.png')}}" >
+            </div>
+            </footer>
+        </div>
+
+        </div>
+      
+</body>
+    <!-- /#wrapper -->
+     <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+	});
+	
+    </script>
+<html>
