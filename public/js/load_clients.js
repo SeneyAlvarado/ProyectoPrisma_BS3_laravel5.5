e50df4cc0cacabfb62e1ab2f3c0eec9dd2e5bc96
YerickRamirez <yerick.ramirez@ucrso.info>
2019-06-20 15:06:15 -0600

    ordenes

--- public/js/load_clients.js
+++ public/js/load_clients.js
@@ -6,7 +6,7 @@ function listClients(){
    dataType: "json",
    success:function(datos){ 
    $('#selectList').empty();
-   $('#selectList').append("<option value='defecto'>Seleccione un cliente</option>");  
+   $('#selectList').append("<option value='defecto'  selected='selected'>Seleccione un cliente</option>");  
    var identification; 
    $.each(datos, function()
    {
@@ -34,10 +34,46 @@ function listClients(){
    });
 }
 
+function listClients_contact(){
+    $.ajax({
+   url: '/fillnames',
+   type: 'GET',
+   dataType: "json",
+   success:function(datos){ 
+   $('#selectList_contact').empty();
+   $('#selectList_contact').append("<option value='defecto' selected='selected'>Seleccione un cliente</option>");  
+   var identification; 
+   $.each(datos, function()
+   {
+    
+       $.each(this, function(){
+           if(this.identification == null) {
+                identification = "No posee";
+           } else {
+                identification = this.identification;
+           }
+           if (this.type == 1){
+            $('#selectList_contact').append('<option value="' + this.id + '">' + this.id + ". " + identification + " " + this.name + " " + this.lastname + " " + this.second_lastname + '</option>');
+           } else {
+            $('#selectList_contact').append('<option value="' + this.id + '">' + this.id + ". " + identification + " " + this.name + '</option>');
+           }
+           
+       }) 
+       
+   })
+
+   }, error:function() {
+       alert("¡Ha habido un error! Elija correctamente la sucursal." +
+       "Si este error persiste por favor comuníquese con el equipo técnico");   
+   }
+   });
+}
+
 
 $(document).ready(function(){
     alert("documentknsddsf");
     listClients();
+    listClients_contact();
 })
 
 
--- resources/views/admin/orders/create.blade.php
+++ resources/views/admin/orders/create.blade.php
@@ -1,7 +1,13 @@
 <script src="{{asset('js/load_clients.js')}}"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" />
 @extends('masterAdmin')
 @section('contenido_Admin')
 <script src="{{asset('js/load_branches_admin.js')}}"></script>
+<script src="{{asset('js/check_clients_branch_select.js')}}"></script>
 <link rel="stylesheet" type="text/css" href="{{asset('css/botonesCrear.css')}}">
 <div style="padding:10px;">
         <div class="card">
@@ -9,60 +15,97 @@
             <div class="card-body">
             <div class="container-fluid">
                 <div class="">
-                    <form method='POST' action=''>
+                    <form method='POST' action='{{ route("orders.store") }}' onsubmit="return check_clients_branch_select(this)">
                         <input type='hidden' name='_token' value='{{Session::token()}}'>
 
                         <div class="row justify-content-center">
                             <div class="col-md-8 align-self-center" >
                                 <label for="name"><strong>Cliente</strong></label>
-                                <select multiple class="form-control" id="selectList" name="selectList">
+                                <select multiple class="form-control" id="selectList" name="owner_client">
+     
+                                </select>  
+                            </div>
+    
+                        </div>
+                        <div class="row justify-content-center">
+
+                            <div class="col-md-8 align-self-center" >
+                                <label for="name"><strong>Cliente de contacto</strong></label>
+                                <select multiple class="form-control" id="selectList_contact" name="contact_client">
      
                                 </select>  
                             </div>
                         </div>
-                        <div class="row justify-content-center">
-                            <div class="col-md-4 " style="margin-top:15px;">
-                                <label for="name"><strong>Segundo apellido</strong></label>
-                                <input id="second_lastname" placeholder="Segundo Apellido" class="form-control" name="second_lastname" type="text" pattern="[a-zA-Z-ñÑáéíóúÁÉÍÓÚ \s]{2,48}" title="No se permiten números en este campo" required>
+
+                        <div class="row justify-content-center" style="margin-top:20px;">
+                            <div class="col-md-4 " >
+                                <label for="name"><strong>Detalle de orden</strong></label>
+                                <input id="order_detail" placeholder="Detalle de la órden" class="form-control" name="order_detail" type="text" pattern="[a-zA-Z-ñÑáéíóúÁÉÍÓÚ0-9 \s]{2,48}" title="" required>
                             </div>
-                            <div class="col-md-4 " style="margin-top:15px;">
-                                <label for="user_name"><strong>Nombre de usuario</strong></label>
-                                <input id="user_name" placeholder="Nombre de usuario" class="form-control" name="user_name" type='text' required>
+                            <div class="col-md-4 ">
+                                <label for="user_name"><strong>Número Cotización</strong></label>
+                                <input id="user_name" placeholder="# Cotización" class="form-control" name="user_name" type='text' pattern="[0-9]*" title="No se permite ingresar letras en este campo" required>
                             </div>
                         </div>
                         <div class="row justify-content-center">
                             <div class="col-md-4 offset-md-5">
-                            @if ($errors->has('user_name'))
+                            @if ($errors->has('delibery_date'))
                             <span class="help-block">
-                                <strong style="color:red;">{{ $errors->first('user_name') }}</strong>
+                                <strong style="color:red;">{{ $errors->first('delibery_date') }}</strong>
                             </span>
                             @endif
                             </div>
                         </div>
-                        <div class="row justify-content-center">
-                        <div class="col-md-4 " style="margin-top:15px;">
-                            <label for="email"><strong>Correo electrónico</strong></label>
-                            <input id="email" placeholder="Correo electrónico" class="form-control" name="email" type='email' required>
-                        </div>
-                        <div class="col-md-4" style="margin-top:15px;">
+                        <div class="row justify-content-center" style="margin-top:20px;">
+                        <div class="col-md-4" >
+								<label for="date-field"><strong>Fecha de entrega</strong></label>
+								<div class="input-group date" id="datetimepicker4" data-target-input="nearest">
+									<input type="text"  name="date" class="form-control datetimepicker-input" data-target="#datetimepicker4"/>
+									<div class="input-group-append" data-target="#datetimepicker4" data-toggle="datetimepicker">
+										<div class="input-group-text"><span class="glyphicon glyphicon-calendar"></span></div>
+									</div>
+								</div>
+								<script type="text/javascript">
+									$(function () {
+										$('#datetimepicker4').datetimepicker({
+											format: 'dd/mm/yyyy',
+											defaultDate: new Date()
+										});
+									});
+								</script>
+
+							</div>
+                            <div class="col-md-4">
                             <label for="branch"><strong>Sucursal</strong></label>
                             <select id="dropBranch" name="dropBranch" class="form-control"></select>
                         </div>
+                        
                         </div>
-                        <div class="row">
-                            <div class="col-md-4 offset-md-2" style="margin-top:15px;">
-                                <label for="name"><strong>Confirmar contraseña</strong></label>
-                                <input placeholder="Confirmar contraseña" id="password_confirmation" type="password" class="form-control" name="password_confirmation" required minlength="1">
+                        <div class="row" style="margin-top:20px;">
+                        <div class="col-md-3 offset-md-2" >
+                            <label for="branch"><strong>¿Posee adelanto económico?</strong></label>
+                            <div class="input-group " id="sexo">
+                                <span class="radio">
+                                <label>Sí</label>
+                                <label>
+                                    <input type="radio" value="1" class="radiobox" name="payment">
+                                </label>
+                               
+                                    <label>
+                                    <label style="margin-left:20px;">No</label>
+                                        <input type="radio" value="2" class="radiobox" name="payment" checked>
+                                    </label>
+                                </span>
                             </div>
                         </div>
-                        
-                        <br>
+                        </div>
+                     
                         <div class="row justify-content-center">
                         <div class="col-md-4 col-md-offset-2" style="margin-top:5px;  ">
                             <button class='btn btn-success btn-block' type='submit'><i class="fa fa-floppy-o"></i> Guardar</button>
                         </div>
                         <div class="col-md-4" style="margin-top:5px; ">
-                            <a class="btn btn btn-block" href="{{ route('user.index') }}">Cancelar</a>
+                            <a class="btn btn btn-block" href="{{ route('products') }}">Cancelar</a>
                         </div>
                         </div>
                         <!--<a style="margin-top: 5px;" href="/especialistas" class = 'btn btn-primary'><i class="fa fa-home"></i>Ver Especialistas</a>-->
@@ -78,9 +121,13 @@
 
 $(document).ready(function(){
     listClients();
+    listClients_contact();
 })
 
-
+$('#datepicker').datepicker({
+  uiLibrary: 'bootstrap4',
+  locale: 'es-es',
+});
 </script>
 
 @endsection
