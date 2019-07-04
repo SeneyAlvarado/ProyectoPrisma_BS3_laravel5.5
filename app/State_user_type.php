f77775b8b664de3c014276ff5ca3e9c4d235fac1
YerickRamirez <yerick.ramirez@ucrso.info>
2019-07-03 21:41:18 -0600

    Ver trabajos en disitintos m[odulos

--- app/State_user_type.php
+++ app/State_user_type.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class State_user_type extends Model
 {
     protected $fillable = ['states_id', 'user_types_id', 'state_notification', 
-    'active_flag', 'state_name', 'user_type_name'];
+    'active_flag', 'state_name', 'user_type_name', 'view_state', 'edit_state' ];
 
     public $timestamps = false;
 }
